e2e3bbb384b25c8bc1c7bcf8967283c8ba9e7140
Anouar LAACHIR <become.engineer88@gmail.com>
2024-10-29 21:58:16 +0100

    changing variable value and affecting varibale value to another variable

--- src/main/java/com/codewa/live02/Variables.java
+++ src/main/java/com/codewa/live02/Variables.java
@@ -7,5 +7,13 @@ public class Variables {
 
         //print the value of age
         System.out.println("Your age is " + age);
+
+        //changing the value of age
+        age = 60;
+        System.out.println("Your age is " + age);
+
+        //affect the value of age to another variable
+        int herAge = age;
+        System.out.println("Her age is " + herAge);
     }
 }
