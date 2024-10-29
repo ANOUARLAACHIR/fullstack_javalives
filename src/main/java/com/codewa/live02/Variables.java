a48a6c2891898b68bc2d10906d23b4f6aca8620a
Anouar LAACHIR <become.engineer88@gmail.com>
2024-10-29 21:53:13 +0100

    variables in java

---
+++ src/main/java/com/codewa/live02/Variables.java
@@ -0,0 +1,11 @@
+package com.codewa.live02;
+
+public class Variables {
+    public static void main(String[] args) {
+        //declaration + initialization
+        int age = 33;
+
+        //print the value of age
+        System.out.println("Your age is " + age);
+    }
+}
