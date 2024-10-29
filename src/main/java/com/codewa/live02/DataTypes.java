e517ff0e355db5762c3136ec7764800cf47a5a53
Anouar LAACHIR <become.engineer88@gmail.com>
2024-10-29 22:18:40 +0100

    primitive data types in java

---
+++ src/main/java/com/codewa/live02/DataTypes.java
@@ -0,0 +1,20 @@
+package com.codewa.live02;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        //in java we have 8 primitive data types
+        //char, byte, short, int, long, float, double and boolean
+        //characters simple quote (')
+        char questionMark = '?';
+        //integers
+        byte age = 33;
+        short year = 2024;
+        int salary = 2_003_003;
+        long population = 8_100_000_00L;
+        //floating point numbers
+        double currency = 10.33;
+        float weight = 70.9F;
+        //boolean
+        boolean isRaining = true;
+ }
+}
